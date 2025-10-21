2561f603a3b6111505a2c7d0de9b069c9b0c16e1
Kenechi <caseynzewi@gmail.com>
2025-10-21 13:32:34 +0100

    fix startup issues

--- main.py
+++ main.py
@@ -77,3 +77,11 @@ def get_profile(request: Request):
     }
 
     return JSONResponse(content=data, media_type="application/json")
+
+import os
+import uvicorn
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
+
